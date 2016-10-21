6d7df7572a463ba48a9b5b667a77e8749a99c8f9
AndroPlus <hosigumayuugi@gmail.com>
2016-10-21 11:42:09 +0900

    state_notifier: fix build

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -2537,7 +2537,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
 			state_resume();
 #endif
 		break;
@@ -2555,7 +2554,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		rc = mdss_dsi_off(pdata, power_state);
 		mutex_unlock(&ctrl_pdata->dsi_ctrl_mutex);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
 			state_suspend();
 #endif
 		break;
